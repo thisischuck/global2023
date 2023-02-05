923c150c056cd50384c25989a92af52cc0a2ea5b
Torres-Git <luismigueltorres@outlook.pt>
2023-02-05 14:05:00 +0000

    bk mole reference fix

--- Assets/_Developers/Mrmav/BackgroundEffect.cs
+++ Assets/_Developers/Mrmav/BackgroundEffect.cs
@@ -18,7 +18,7 @@ public class BackgroundEffect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Mole = GameManager.Instance.Player;
     }
 
     void LateUpdate()
